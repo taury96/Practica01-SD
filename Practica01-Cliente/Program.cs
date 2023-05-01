88700e7efe57e1a6221b72ec145a6b18e70a1a5c
taurydes <taurydes@gmail.com>
2023-05-01 15:17:56 -0400

    Practica Funcionando

---
+++ Practica01-Cliente/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClienteSocket
+{
+    class Cliente
+    {
+        static void Main(string[] args)
+        {
+            StartClient();
+        }
+
+        public static void StartClient()
+        {
+            // Establecer el endpoint para el socket
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000); //<- cambiar 
+
+            //ESta para conectarme a la dirección del servidor : 
+            //IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse("192.168.0.100"), 5000);//<-- elegir la dirección IP del servidor
+            /*Esto hará que el cliente intente conectarse al servidor en la dirección IP especificada. 
+             * Si el servidor está correctamente configurado y en ejecución en esa dirección IP, la conexión se establecerá correctamente.*/
+
+
+// Crear un socket TCP/IP
+Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+try
+{
+    // Conectar el socket al endpoint remoto
+    sender.Connect(endpoint);
+
+    Console.WriteLine("Conexión establecida con el servidor.");
+
+    // Enviar datos al servidor
+    string message = "";
+    while (message != "x")
+    {
+        Console.WriteLine("Escriba un mensaje para enviar al servidor:");
+        message = Console.ReadLine();
+        byte[] data = Encoding.ASCII.GetBytes(message);
+        sender.Send(data);
+
+        // Recibir la respuesta del servidor
+        byte[] buffer = new byte[1024];
+        int bytesRec = sender.Receive(buffer);
+        string response = Encoding.ASCII.GetString(buffer, 0, bytesRec);
+        Console.WriteLine("Respuesta recibida del servidor: " + response);
+    }
+
+    // Cerrar el socket
+    sender.Shutdown(SocketShutdown.Both);
+    sender.Close();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
+}
+}
+}
---
+++ Practica1-Servidor/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ServidorSocket
+{
+    class Servidor
+    {
+        static void Main(string[] args)
+        {
+            StartServer();
+        }
+
+        public static void StartServer()
+        {
+            // Establecer el endpoint para el socket
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, 5000); // <--Cambiar
+
+            // Cambio requerido para conectarme desde otro equipo :
+            // IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse("192.168.1.100"), 5000); //<-- funciona con otros equipos
+            //De esta manera, el servidor estará escuchando conexiones en la dirección IP 192.168.1.100 en el puerto 5000.
+          
+            
+            
+            // Crear un socket TCP/IP
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // Vincular el socket a un endpoint y escuchar por conexiones
+            listener.Bind(endpoint);
+            listener.Listen(2);
+
+            Console.WriteLine("Servidor socket iniciado. Esperando conexiones...");
+
+            try
+            {
+                while (true)
+                {
+                    // Esperar por una conexión
+                    Socket handler = listener.Accept();
+
+                    Console.WriteLine("Conexión aceptada desde " + handler.RemoteEndPoint.ToString());
+
+                    // Iniciar un nuevo hilo para manejar la conexión del cliente
+                    Thread thread = new Thread(() => HandleClient(handler));
+                    thread.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public static void HandleClient(Socket handler)
+        {
+            try
+            {
+                byte[] buffer = new byte[1024];
+                string data = null;
+
+                while (true)
+                {
+                    int bytesRec = handler.Receive(buffer);
+                    data += Encoding.ASCII.GetString(buffer, 0, bytesRec);
+                    Console.WriteLine("Mensaje recibido del cliente: " + data);
+
+                    if (data.IndexOf("<EOF>") > -1)
+                    {
+                        break;
+                    }
+
+                    byte[] response = Encoding.ASCII.GetBytes("Respuesta del servidor: " + data);
+                    handler.Send(response);
+                }
+
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
